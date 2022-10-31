4567f5641d7fc78ba49221b5c0e43daee394557a
Fernando Espinoza <eze980523@gmail.com>
2022-10-31 20:44:40 -0300

    Correciones de codigo

--- main.js
+++ main.js
@@ -1,83 +1,109 @@
-const DADO = Math.round(Math.random()*6)
 
-function Resultado(Tirar){
-if ( valorDelDado = DADO ){
-     apuesta + saldo;
-} else{
-        saldo -= apuesta;
-}
-console.log(saldo)
-seleccionarOperacion()
-
-}
+let apuesta;
+let valorDelDado; 
+let saldo=0
+let DADO;
 
 
-function Tirar(){
-    console.log(DADO);
-    Resultado()
+validarEdad()
+
+function validarEdad(){
+    let advertencia = confirm("Continue sólo si es mayor.") 
+    ingresarEdad(advertencia)
 }
 
-function Jugar(){
-    console.log("El juego consiste en una tirada de dados,si usted acierta recibe un duplicado de lo que aposto,de lo contrario se le descontara de su saldo ")
-    let apuesta = Number(prompt("Ingrese cuánto desea apostar"))
-    let valorDelDado = Number(prompt("Ingrese el valor del dado a salir"))
-    Tirar()
+function ingresarEdad(advertencia) {
+    if(advertencia){
+        let edad = Number(prompt("Ingrese su edad por favor."))
+        
+        confirmarEdad(edad)
+        } else{
+            alert("Lo sentimos,necesitamos verificar su edad.")
+        }
+    
 }
-function IngresarDinero (){
-    let saldo = Number(prompt("Ingrese su dinero"))
-    alert ("Su saldo a sido ingreado con éxito")
-    console.log("Su saldo es "+ saldo )
-    seleccionarOperacion()
+function confirmarEdad (edad){
+    
+    if ( edad >= 18){
+        seleccionarOperacion()
+    }else{
+        alert("Lo sentimos,usted no puede ingresar.")
+        validarEdad()
+    }
 }
-function Salir(){
-    alert("Fin del proceso")
-}
+
+   
 
 
 function seleccionarOperacion (){
-    console.log("1-----Jugar")
-    console.log("2-----Ingresar dinero")
-    console.log("3-----Salir")
+    console.log("1-----Jugar.")
+    console.log("2-----Ingresar dinero.")
+    console.log("3-----Consultar saldo.")
+    console.log("4-----Salir.")
     console.log("----------------")
     
-    let operacion = Number(prompt("Que deseas realizar"))
+    let operacion = Number(prompt("Que deseas realizar?"))
     switch(operacion){
         case 1:
             Jugar()
             break
-    case 2:
-          IngresarDinero()
-        break 
+        case 2:
+            IngresarDinero()
+            break 
         case 3:
-            Salir()
+            DineroEnCuenta() 
             break
+        case 4:
+            Salir()   
         default:
             console.log("Operación invalida,intente nuevamente.")
     }
 }
 
-function confirmarEdad (edad){
-    
-    if ( edad >= 18){
-     seleccionarOperacion()
-    }else{
-        alert("Lo sentimos,usted no puede ingresar.")
+function Jugar(){
+    console.log("El juego consiste en una tirada de dados,si usted acierta recibe un duplicado de lo que aposto,de lo contrario se le descontara de su saldo.")
+    if(saldo==0){
+        alert("Lo sentimos,debe tener un saldo para jugar.")
+        seleccionarOperacion()
+    }else{}
+    Tirar()
+}
+function IngresarDinero (){
+    saldo = Number(prompt("Ingrese su dinero."))
+    alert ("Su saldo a sido ingreado con éxito.")
+    console.log("Su saldo es $"+ saldo )
+    seleccionarOperacion()
+}
+function DineroEnCuenta(){
+    console.log("Su saldo es de $"+saldo)
+    seleccionarOperacion()
+}
+function Salir(){
+    alert("Fin del proceso.")
+}
+function TirarDado(){
+     DADO = Math.round(Math.random()*6)
+}
+
+function Tirar(){
+     apuesta = Number (prompt("Ingrese cuánto desea apostar." ))
+     if(apuesta>saldo){ 
+                  alert("Lo sentimos,pero no puede apostar mayor a su saldo actual.")
+                  seleccionarOperacion()
+     } else {}
+     valorDelDado = Number (prompt("Ingrese el valor del dado a salir." ))
+     TirarDado()
+     console.log("El valor del dado es "+DADO);
+     Resultado()
+                         }
+
+function Resultado(DADO){
+    if ( valorDelDado == DADO ){
+        saldo = apuesta + saldo;
+    } else{
+        saldo = saldo -= apuesta;
     }
-}
-
-
-function ingresarEdad(advertencia) {
-    if(advertencia){
-        let edad = Number(prompt("Ingrese su edad por favor."))
-        confirmarEdad(edad)
-        } else{
-            alert("Es necesario que verifiquemos su edad.")
-        }
- 
-}
-function validarEdad(){
-    let advertencia = confirm("continue solo si es mayor") 
-    ingresarEdad(advertencia)
-}
-
-validarEdad()
+    console.log("Su saldo es $"+saldo)
+    console.log("")
+    seleccionarOperacion()
+    }
